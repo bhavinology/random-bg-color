5684c326d66596e7f79d012dc2cee3c0cfe56d50
Bhavin <bhavinpatel9624@gmail.com>
2024-03-15 23:39:06 +0530

    function for random hex color code generation added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function randomColor() {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function changecolor() {
   setInterval(() => {
-    document.body.style.background = "green"; // write a function which gives random hex color code
+    document.body.style.background = randomColor(); // write a function which gives random hex color code
   }, 1000);
 }
 
